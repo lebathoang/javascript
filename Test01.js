4e626c84e8725650869da3edbed2d4a4fd3183d6
Phan Thanh <phanthanh.humg@gmail.com>
2022-03-22 09:03:14 +0700

    Update Test01.js

--- Test01.js
+++ Test01.js
@@ -1,4 +1,5 @@
-Câu hỏi 01: Cho 1 số nguyên x và a là mảng với N số nguyên
-Viết 1 function lấy ra các mảng thỏa mãn điều kiện tổng 2 số a[i] + a[j] = x
-Ví dụ: có mảng [3, 5, 2, -4, 8, 11] và x = 7.
-function phải trả ra được kết quả là [[11, -4], [2, 5]] vì 11 + -4 = 7 và 2 + 5 = 7
+/* Câu hỏi 01: Cho 1 số nguyên x và a là mảng với N số nguyên
+* Viết 1 function lấy ra các mảng thỏa mãn điều kiện tổng 2 số a[i] + a[j] = x
+* Ví dụ: có mảng [3, 5, 2, -4, 8, 11] và x = 7.
+* function phải trả ra được kết quả là [[11, -4], [2, 5]] vì 11 + -4 = 7 và 2 + 5 = 7
+*/
