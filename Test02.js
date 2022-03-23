2c96086f5c734d4373160ecd83cffa578fb81d6a
Phan Thanh <phanthanh.humg@gmail.com>
2022-03-23 10:51:58 +0700

    Create Test02.js

---
+++ Test02.js
@@ -0,0 +1,4 @@
+/**
+* Viết 1 function có 2 tham số đầu vào: a là mảng bất kỳ, x là 1 số nguyên bất kỳ.
+* Check nếu mảng có 3 phần tử liên tiếp có tổng bằng x thì trả ra true, ngược lại trả ra false
+*/
