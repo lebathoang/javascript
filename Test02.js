0bc5d81043aff44e27090c4107790b353d0cdf10
Phan Thanh <phanthanh.humg@gmail.com>
2022-03-23 11:15:40 +0700

    Update Test02.js

--- Test02.js
+++ Test02.js
@@ -2,3 +2,14 @@
 * Viết 1 function có 2 tham số đầu vào: a là mảng bất kỳ, x là 1 số nguyên bất kỳ.
 * Check nếu mảng có 3 phần tử liên tiếp có tổng bằng x thì trả ra true, ngược lại trả ra false
 */
+
+/**
+* Cho 1 mảng có các phần tử là mảng số nguyên, vd: [[1,2],[3,0],[-4]]
+* Tính tổng các số nguyên của các mảng phần tử. vd kết quả ở trên sẽ là 2. Vì 1+2+3+0+(-4) = 2
+* Yêu cầu: Giải với "reduce" và không dùng "reduce"
+*/
+
+/**
+* Viết 1 function 2 tham số đầu vào là 2 chuỗi X, Y
+* Kiểm tra xem một chuỗi X có phải là một vòng quay của một chuỗi Y hay không. Ví dụ. ABCD là phép quay của BCDA nhưng không phải của ACBD.
+*/
